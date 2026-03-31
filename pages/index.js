e0db007ba9a960ce942d31939566115a03afe7a8
parth <parth@advant.xyz>
2026-03-31 20:23:30 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,44 @@
+import path from "path";
+import fs from "fs/promises";
+
+//this run second at end
+function HomePage(props) {
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+    </ul>
+  );
+}
+
+//this run first
+export async function getStaticProps(context) {
+  console.log("(Re-)Generating..");
+  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filePath);
+  const data = JSON.parse(jsonData);
+
+  if (!data) {
+    return {
+      redirect: {
+        destination: "/no-data",
+      },
+    };
+  }
+
+  if (data.products.length === 0) {
+    return { notFound: true };
+  }
+
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
+}
+
+export default HomePage;
